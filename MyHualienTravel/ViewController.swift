0c68176d6d86c55fee998ac9da3a18e5c00cadcf
Chou <tomorrow@chous-imac.local>
2017-12-29 17:32:32 +0800

    Initial Commit

---
+++ MyHualienTravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHualienTravel
+//
+//  Created by Chou on 2017/12/29.
+//  Copyright © 2017年 NQU110410545. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
