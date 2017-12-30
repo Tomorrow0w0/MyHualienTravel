f58f1a7963ecfd858f1a59f89e6f3ee6b8446675
Chou <tomorrow@chous-imac.local>
2017-12-30 19:07:31 +0800

    完成圖片上傳至Firebase，但圖片尚未顯示出來。

---
+++ MyHualienTravel/ImageGalleryCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageGalleryCell.swift
+//  MyHualienTravel
+//
+//  Created by Chou on 2017/12/30.
+//  Copyright © 2017年 NQU110410545. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var fireImageView: UIImageView!
+    
+}
